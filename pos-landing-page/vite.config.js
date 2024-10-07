91e9e1a838725580b83f7dd7d2052cd61b3b8e67
ugfrds <justlikewiseman@gmail.com>
2024-10-07 16:54:48 +0300

    changes for vite
    changes for vite

--- pos-landing-page/vite.config.js
+++ pos-landing-page/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pos-website/', // This ensures the correct base path for GitHub Pages
   plugins: [react()],
 })
