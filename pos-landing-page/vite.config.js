d247e2cff194cc9ff7adf4cfb60e0b3edac0eb9e
ugfrds <justlikewiseman@gmail.com>
2024-12-09 17:51:14 +0300

    updated website

--- pos-landing-page/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // This ensures the correct base path for GitHub Pages
-  plugins: [react()],
-})
